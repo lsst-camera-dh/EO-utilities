69e32ea09ada1de17e41d5d50f2fef15302986c1
Eric Charles <echarles@gmail.com>
2019-04-26 14:38:47 -0700

    Added bin/eo_task.py

--- bin/eo_task.py
+++ bin/eo_task.py
@@ -2,7 +2,7 @@
 
 """This module is just a command line interface to plot the FFT of bias images"""
 
-from lsst.eo_utils.base.analysis import EO_TASK_FACTORY
+from lsst.eo_utils.base.factory import EO_TASK_FACTORY
 
 from lsst.eo_utils.bias import bias_fft, bias_v_row, bias_struct,\
     oscan_amp_stack, correl_wrt_oscan, superbias, superbias_stats
