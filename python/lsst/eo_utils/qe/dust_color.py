8409425500a540b12b33a8d07ff235fb7d224cf1
Eric Charles <echarles@gmail.com>
2019-08-19 11:15:49 -0700

    Updated DustColor task

--- python/lsst/eo_utils/qe/dust_color.py
+++ python/lsst/eo_utils/qe/dust_color.py
@@ -2,6 +2,8 @@
 
 import sys
 
+from lsst.eo_utils.base.defaults import ALL_SLOTS
+
 from lsst.eo_utils.base.config_utils import EOUtilOptions
 
 from lsst.eo_utils.base.data_utils import TableDict, construct_bbox_dict
@@ -12,14 +14,18 @@ from lsst.eo_utils.base.iter_utils import AnalysisBySlot
 
 from lsst.eo_utils.base.image_utils import get_ccd_from_id, get_amp_list,\
     get_exposure_time, get_mondiode_val, get_mono_wl,\
-    get_geom_regions, unbias_amp, get_raw_image
+    get_geom_regions, get_raw_image, unbias_amp
 
 from lsst.eo_utils.base.factory import EO_TASK_FACTORY
 
 from lsst.eo_utils.sflat.file_utils import RAFT_SFLAT_TABLE_FORMATTER
 
+from lsst.eo_utils.qe.file_utils import SLOT_QE_TABLE_FORMATTER,\
+    RAFT_QE_TABLE_FORMATTER, RAFT_QE_PLOT_FORMATTER
+
 from lsst.eo_utils.qe.analysis import QeAnalysisConfig, QeAnalysisTask
 
+import numpy as np
 
 class DustColorConfig(QeAnalysisConfig):
     """Configuration for DustColorTask"""
@@ -30,7 +36,7 @@ class DustColorConfig(QeAnalysisConfig):
 
 
 class DustColorTask(QeAnalysisTask):
-    """Analyze some monochromatic data to extract the wavelength dependence of defects and dusk spots"""
+    """Analyze some qe data"""
 
     ConfigClass = DustColorConfig
     _DefaultName = "DustColorTask"
@@ -66,8 +72,11 @@ class DustColorTask(QeAnalysisTask):
         self.safe_update(**kwargs)
 
         slot = self.config.slot
-
         qe_files = data['LAMBDA']
+        lams = []
+        for file in qe_files:
+            lam = file.split('flat_')[1].split('_')[0]
+            lams.append(lam)
 
         mask_files = self.get_mask_files()
         superbias_frame = self.get_superbias_frame(mask_files)
@@ -82,50 +91,88 @@ class DustColorTask(QeAnalysisTask):
 
         # This is a dictionary of dictionaries to store all the
         # data you extract from the qe_files
-        data_dict = dict(WL=[], EXPTIME=[], MONDIODE=[])
+        data_dict = dict(SLOT=[], AMP=[], XCORNER=[], YCORNER=[], XSIZE=[], YSIZE=[])
+        for lam in lams:
+            data_dict['FLUX_' + lam] = []
+            data_dict['MED_' + lam] = []
+
+        temp_list = []
 
         # Analysis goes here, you should fill data_dict with data extracted
         # by the analysis
         #
+        # For this slot, loop over qe files, loop over amplifiers, loop over dust spots in that amplifier,
+        #  record the flux and median for each dust spot
+        # Then loop over dust spots CCDs, amplifiers, and QE files to accumulate the output dictionary
+        ccd = get_ccd_from_id(butler, qe_files[0], mask_files)
+        amps = get_amp_list(ccd)
+        spot = 0
         for ifile, qe_file in enumerate(qe_files):
-            if ifile % 10 == 0:
-                self.log_progress("  %i" % ifile)
+            lam = qe_file.split('flat_')[1].split('_')[0]
+            #for i, amp in enumerate(amps):
+            for i, amp in enumerate(slot_bbox_dict.keys()):
+                bbox_list = slot_bbox_dict[amp]
+                
+                if len(bbox_list) > 100:
+                    self.log.warn("Skipping slot:amp %s:%i with %i defects" % (slot, amp, len(bbox_list)))
+                    continue
 
-            ccd = get_ccd_from_id(butler, qe_file, mask_files)
-
-            data_dict['WL'].append(get_mono_wl(ccd))
-            data_dict['EXPTIME'].append(get_exposure_time(ccd))
-            data_dict['MONDIODE'].append(get_mondiode_val(ccd))
-
-            amps = get_amp_list(ccd)
-            for i, amp in enumerate(amps):
-
-                bbox_list = slot_bbox_dict[i]
+                ccd = get_ccd_from_id(butler, qe_file, mask_files)
                 regions = get_geom_regions(ccd, amp)
                 serial_oscan = regions['serial_overscan']
                 imaging = regions['imaging']
-
-                superbias_im = self.get_superbias_amp_image(butler, superbias_frame, amp)
-                img = get_raw_image(ccd, amp)
+                img = get_raw_image(ccd, amp + 1)
+                if superbias_frame is not None:
+                    if butler is not None:
+                        superbias_im = get_raw_image(superbias_frame, amp + 1)
+                    else:
+                        superbias_im = get_raw_image(superbias_frame, amp + 1) 
+                else:
+                    superbias_im = None
 
                 image = unbias_amp(img, serial_oscan, bias_type=self.config.bias,
                                    superbias_im=superbias_im, region=imaging)
+                for bbox in bbox_list:
+                    if ifile == 0:
+                        data_dict['SLOT'].append(slot)
+                        #data_dict['LAM'].append(lam)
+                        data_dict['AMP'].append(amp)
+                        data_dict['XCORNER'].append(bbox.getBeginX())
+                        data_dict['YCORNER'].append(bbox.getBeginY())
+                        data_dict['XSIZE'].append(bbox.getWidth())
+                        data_dict['YSIZE'].append(bbox.getHeight())
 
-                max_bbox = min(len(bbox_list), 10)
-                for bbox in bbox_list[0:max_bbox]:
                     try:
                         cutout = image[bbox]
-                    except Exception:
-                        print("cutout failed", slot, amp, bbox)
-                    print(ifile, bbox, cutout)
+                    except:
+                        pass
+                    # Here evaluate the 'flux' of the feature, relative to the median
+                    # value of the amplifier image.  May also want to assemble bounding
+                    # box corners into a ds9 region file, CCD by CCD
+                    med = np.median(image.array)
+                    flux = np.sum(cutout.array) - bbox.getWidth()*bbox.getHeight()*med
+                    spot += 1
+                    #temp_dict[spot].append((lam, flux, med))
+                    temp_list.append((lam, flux, med))
+
+        for i in range(len(temp_list)):
+            #data_dict['SLOT'].append(temp_dict['SLOT'][i])
+            #data_dict['AMP'].append(temp_dict['AMP'][i])
+            #lam = temp_dict['LAM'][i]
+            lam, flux, med = temp_list[i]
+            data_dict['FLUX_' + lam].append(flux)  #temp_dict['FLUX'][i])
+            data_dict['MED_' + lam].append(med)  #np.median(image.array))
 
 
         sys.stdout.write("!\n")
         sys.stdout.flush()
+        for key in data_dict.keys():
+            print(key, len(data_dict[key]))
 
         dtables = TableDict()
         dtables.make_datatable('files', make_file_dict(butler, qe_files))
-        dtables.make_datatable('dusk_color', data_dict)
+        #dtables.make_datatable('dust_color', data_dict)
+        dtables.make_datatable('dust_color_hack', data_dict)
 
         return dtables
 
